bd98d1ba9fb763ff3094ff86e489533cc0c9427b
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-08-01 08:51:35 +0800

    fix help message

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ buildj :::create --java<version> --maven<version>      - create java-version, ma
 buildj :::create --java<version> --gradle<version>     - create java-version, gradle-version project
 buildj :::java<version> [-version]                     - run java with assigned version, e.g. buildj :::java1.8 -version
 buildj :::maven<version> [--java<version>]             - run maven with assigned version and java version, e.g. buildj :::maven3.5.2 --java1.8 ARGS
-buildj :::gradle<version> ]--java<version>]            - run gradle with assigned version and java version, e.g. buildj :::gradle3.5.1 --java1.8 ARGS
+buildj :::gradle<version> [--java<version>]            - run gradle with assigned version and java version, e.g. buildj :::gradle3.5.1 --java1.8 ARGS
 buildj                                                 - run build, run assigned version builder tool
 "#);
 }
