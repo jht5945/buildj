eb9f5afc16fb0d3d05e27ec92a55dff8cdb079c3
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-08-11 11:25:55 +0800

    update main.rs

--- src/misc.rs
+++ src/misc.rs
@@ -7,19 +7,19 @@ lazy_static! {
 
 pub fn print_usage() {
     print!(r#"
-buildj :::                                             - print this message
-buildj :::help                                         - print this message
-buildj :::create --java<version> --maven<version>      - create java-version, maven-version project
+buildj :::                                           - print this message
+buildj :::help                                       - print this message
+buildj :::create --java<version> --maven<version>    - create java + maven project
   e.g. buildj :::create --java1.8 --maven3.5.2
-buildj :::create --java<version> --gradle<version>     - create java-version, gradle-version project
+buildj :::create --java<version> --gradle<version>   - create java + gradle project
   e.g. buildj :::create --java1.8 --gradle3.5.1
-buildj :::java<version> [-version]                     - run java with assigned version
+buildj :::java<version> [-version]                   - run java with assigned version
   e.g. buildj :::java1.8 -version
-buildj :::maven<version> [--java<version>]             - run maven with assigned version and java version
+buildj :::maven<version> [--java<version>]           - run maven with assigned version and java version
   e.g. buildj :::maven3.5.2 --java1.8 ARGS
-buildj :::gradle<version> [--java<version>]            - run gradle with assigned version and java version
+buildj :::gradle<version> [--java<version>]          - run gradle with assigned version and java version
   e.g. buildj :::gradle3.5.1 --java1.8 ARGS
-buildj                                                 - run build, run assigned version builder tool
+buildj                                               - run build, run assigned version builder tool
 "#);
 }
 
