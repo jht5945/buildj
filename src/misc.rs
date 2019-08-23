ec06e6e5c424d89a2d7a4e48c9f624fc4c68a911
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-08-24 01:01:24 +0800

    update print_usage

--- src/misc.rs
+++ src/misc.rs
@@ -8,21 +8,21 @@ lazy_static! {
 
 pub fn print_usage() {
     print!(r#"
-buildj :::                                           - print this message
-buildj :::help                                       - print this message
-buildj :::version                                    - print version
-buildj :::create --java<version> --maven<version>    - create java + maven project
+buildj :::                                          - print this message
+buildj :::help                                      - print this message
+buildj :::version                                   - print version
+buildj :::create --java<version> --maven<version>   - create java + maven project
   e.g. buildj :::create --java1.8 --maven3.5.2
-buildj :::create --java<version> --gradle<version>   - create java + gradle project
+buildj :::create --java<version> --gradle<version>  - create java + gradle project
   e.g. buildj :::create --java1.8 --gradle3.5.1
-buildj :::java<version> [-version]                   - run java with assigned version
+buildj :::java<version> [-version]                  - run java with assigned version
   e.g. buildj :::java1.8 -version
-buildj :::maven<version> [--java<version>]           - run maven with assigned version and java version
+buildj :::maven<version> [--java<version>]          - run maven with assigned version and java version
   e.g. buildj :::maven3.5.2 --java1.8 ARGS
-buildj :::gradle<version> [--java<version>]          - run gradle with assigned version and java version
+buildj :::gradle<version> [--java<version>]         - run gradle with assigned version and java version
   e.g. buildj :::gradle3.5.1 --java1.8 ARGS
-buildj                                               - run build, run assigned version builder tool
-BUILDJ_VERBOSE=1 buildj                              - run buildj in verbose mode
+buildj                                              - run build, run assigned version builder tool
+BUILDJ_VERBOSE=1 buildj                             - run buildj in verbose mode
 "#);
 }
 
