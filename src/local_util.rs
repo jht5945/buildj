3c7b4a44244160ad8da6f36809988c112c937ade
Hatter Jiang <jht5945@gmail.com>
2020-05-02 18:46:32 +0800

    ref print_*

--- src/local_util.rs
+++ src/local_util.rs
@@ -11,8 +11,7 @@ use rust_util::{
     XResult,
     new_box_ioerror,
     util_msg::{
-        print_message,
-        MessageType,
+        print_error,
     },
     util_io::*,
 };
@@ -54,7 +53,7 @@ pub fn verify_file_integrity(integrity: &str, file_name: &str) -> XResult<bool> 
             };
             let integrity_verify_result = digest_hex == calc_digest_hex.as_str();
             if ! integrity_verify_result {
-                print_message(MessageType::ERROR, &format!("Verify integrity failed, expected: {}, actual: {}", digest_hex, calc_digest_hex));
+                print_error(&format!("Verify integrity failed, expected: {}, actual: {}", digest_hex, calc_digest_hex));
             }
             Ok(integrity_verify_result)
         },
@@ -134,7 +133,7 @@ pub fn init_home_dir(home_sub_dir: &str) {
 pub fn init_dir(dir: &str) {
     if ! Path::new(dir).exists() {
         fs::create_dir_all(dir).unwrap_or_else(|err| {
-            print_message(MessageType::ERROR, &format!("Init dir {} failed: {}", dir, err));
+            print_error(&format!("Init dir {} failed: {}", dir, err));
         });
     }
 }
