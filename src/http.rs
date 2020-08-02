ddf1c54d5dcf0102c0a5a16622156355fa0e3dde
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2020-08-02 12:01:51 +0800

    style: use rust_util s macros

--- src/http.rs
+++ src/http.rs
@@ -3,17 +3,13 @@ use std::fs::File;
 use rust_util::{
     XResult,
     util_io::copy_io,
-    util_msg::{
-        print_debug,
-        print_warn,
-    },
 };
 
 use super::misc::VERBOSE;
 
 pub fn download_url(url: &str, dest: &mut File) -> XResult<()> {
     if *VERBOSE {
-        print_debug(&format!("Start download URL: {}", url));
+        debugging!("Start download URL: {}", url);
     }
     let mut response = reqwest::get(url)?;
     let header_content_length: i64 = match response.headers().get("content-length") {
@@ -21,20 +17,20 @@ pub fn download_url(url: &str, dest: &mut File) -> XResult<()> {
         Some(len_value) => {
             let len_str = match len_value.to_str() {
                 Ok(len_str) => len_str, Err(err) => {
-                    print_warn(&format!("Get content length for {:?}, error: {}", len_value, err));
+                    warning!("Get content length for {:?}, error: {}", len_value, err);
                     "-1"
                 },
             };
             match len_str.parse::<i64>() {
                 Ok(len) => len, Err(err) => {
-                    print_warn(&format!("Get content length for {:?}, error: {}", len_value, err));
+                    warning!("Get content length for {:?}, error: {}", len_value, err);
                     -1
                 }
             }
         },
     };
     if *VERBOSE {
-        print_debug(&format!("Content-Length: {}", header_content_length));
+        warning!("Content-Length: {}", header_content_length);
     }
     copy_io(&mut response, dest, header_content_length)?;
     Ok(())
@@ -42,7 +38,7 @@ pub fn download_url(url: &str, dest: &mut File) -> XResult<()> {
 
 pub fn get_url_content(url: &str) -> XResult<String> {
     if *VERBOSE {
-        print_debug(&format!("Get URL: {}", url));
+        warning!("Get URL: {}", url);
     }
     Ok(reqwest::get(url)?.text()?)
 }
